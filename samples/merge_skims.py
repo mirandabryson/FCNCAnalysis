7e94a0ad1fa0f7852a5ac5ba0408dd8d1ac4efe7
fgolf <frank.golf@gmail.com>
2021-05-02 13:31:44 -0500

    test

--- samples/merge_skims.py
+++ samples/merge_skims.py
@@ -1,6 +1,6 @@
 import os
-import sys
 import time
+import argparse
 from samples_2016 import samples_2016
 from samples_2017 import samples_2017
 from samples_2018 import samples_2018
@@ -8,13 +8,29 @@ from samples_2018 import samples_2018
 samples = { 2016 : samples_2016, 2017 : samples_2017, 2018 : samples_2018}
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--out", help="output directory", default="/nfs-7/userdata/fgolf/")
+    parser.add_argument("-t", "--tag", help="tag for bookkeeping and output directory location", default="v3.08_allyears_tmp")
+    parser.add_argument("-y", "--year", help="year, if you only want to run one", default="")
+    parser.add_argument(      "--proc", help="process, if you only want to run one/some. accepts wildcards if quoted.", default="", type=str)
+    parser.add_argument(      "--excludeproc", help="opposite of proc", default="", type=str)
+    parser.add_argument(      "--skip_already_done", help="skip procs if already in limits folder", action="store_true")
+    parser.add_argument(      "--slim", help="smaller subset of processes", action="store_true")
+    parser.add_argument(      "--ncpu", help="number of cpus", default=25, type=int)
+    parser.add_argument(      "--maxprocs", help="maximum number of chains", default=-1, type=int)
+    parser.add_argument("-n", "--noloop", help="skip looping/scanchain", action="store_true")
+    parser.add_argument("-v", "--verbosity", help="verbosity level (0 = default,1,2)", default=0, type=int)
+
+    args = parser.parse_args()
+    print type(args)
+    print args
+
     basedirs = {
         2016: "/hadoop/cms/store/user/ksalyer/FCNC_NanoSkim/fcnc_v3/",
         2017: "/hadoop/cms/store/user/ksalyer/FCNC_NanoSkim/fcnc_v3/",
         2018: "/hadoop/cms/store/user/ksalyer/FCNC_NanoSkim/fcnc_v3/",
     }
     outdir = basedirs[2016]
-    if len(sys.argv) > 1: outdir = sys.argv[1]
 
     count=0
     for year in [2016,2017,2018]:
@@ -27,6 +43,7 @@ if __name__ == '__main__':
             ifnames_long = [fpath + f for f in ifnames]
             command='hadd -f %s %s' % (ofname, ' '.join(ifnames_long))
             print command
-            #os.system(command)
             count += 1
-            #if count%5==0: time.sleep(60)
+            if not test:
+                os.system(command)
+                if count%5==0: time.sleep(60)
