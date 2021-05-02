da659b8cdbe2eb239e03ae63b07c390c7b1a1c04
fgolf <frank.golf@gmail.com>
2021-05-02 08:27:55 -0700

    setup script and compilation helpers

---
+++ analysis/compile.C
@@ -0,0 +1,5 @@
+{
+    gROOT->ProcessLine(".L ../../NanoTools/NanoCORE/NANO_CORE.so");
+    gROOT->ProcessLine(".L helpers/histogrammingClass_C.so");
+    gROOT->ProcessLine(".L event_looper.C++");
+}
---
+++ analysis/helpers/compile.C
@@ -0,0 +1,4 @@
+{
+    gROOT->ProcessLine(".L ../../../NanoTools/NanoCORE/NANO_CORE.so");
+    gROOT->ProcessLine(".L histogrammingClass.C++");
+}
