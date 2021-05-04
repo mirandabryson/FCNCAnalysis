a7debc64d722f4903dd7261f338388cc1f6585a9
fgolf <frank.golf@gmail.com>
2021-05-04 17:58:18 -0500

    in progress

--- analysis/tables.py
+++ analysis/tables.py
@@ -1,7 +1,30 @@
-import ROOT
-import time
 import numpy as np
-import sys
-import pandas as pd
-ROOT.TH1F.SetDefaultSumw2()
+import ROOT as r
+r.TH1F.SetDefaultSumw2()
 
+years=[2016,2017,2018]
+procs=['fakes_mc','flips_mc','rares']
+
+sr_hist_prefix='h_br_sr_'
+histdir='outputs/'
+
+def getObjFromFile(fname, hname):
+    f = r.TFile(fname)
+    assert not f.IsZombie()
+    f.cd()
+    htmp = f.Get(hname)
+    if not htmp:  return htmp
+    r.gDirectory.cd('PyROOT:/')
+    res = htmp.Clone()
+    f.Close()
+    return res
+
+yields={}
+for proc in procs:
+    if proc not in yields: yields[proc] = {}
+    for year in years:
+        fname=histdir+proc+'_'+year+'_hists.root'
+        hname=sr_hist_prefix+proc
+        hist=getObjFromFile(fname,hname)
+        yields[proc,year]=np.array([hist->GetArray()->GetArray()],np.dtype=np.float)
+        print '{} {}:'.format(proc,year), yields[proc,year]
