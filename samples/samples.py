9eb9b5fe71125ad4a633307ac9e6037953ce06c9
fgolf <frank.golf@gmail.com>
2021-05-04 12:20:48 -0500

    support different tags more easily

--- samples/samples.py
+++ samples/samples.py
@@ -2,15 +2,15 @@ from samples_2016 import samples_2016
 from samples_2017 import samples_2017
 from samples_2018 import samples_2018
 
-def get_sample_path(sample,year):
+def get_sample_path(sample,year,tag):
     year=int(year)
     if year==2016: 
-        if sample in samples_2016: return samples_2016[sample]
+        if sample in samples_2016: return '{}_{}'.format(samples_2016[sample],tag)
         else: print("ERROR: Sample %s does not exist in year %d." % (sample,year))
     elif year==2017: 
-        if sample in samples_2017: return samples_2017[sample]
+        if sample in samples_2017: return '{}_{}'.format(samples_2017[sample],tag)
         else: print("ERROR: Sample %s does not exist in year %d." % (sample,year))
     elif year==2018: 
-        if sample in samples_2018: return samples_2018[sample]
+        if sample in samples_2018: return '{}_{}'.format(samples_2018[sample],tag)
         else: print("ERROR: Sample %s does not exist in year %d." % (sample,year))
     else: print("ERRROR: No samples for year ", year)
